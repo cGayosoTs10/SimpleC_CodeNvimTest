89c6f9d3dc497ab553bc2c89a7b9bec063e0150a
Cauê Gayoso Tavares da Silva <cauegts11@gmail.com>
2026-03-11 12:19:51 -0300

    testing my new branch

--- src/main.c
+++ src/main.c
@@ -9,4 +9,7 @@ scanf("%d", &num2);
 soma = num1 + num2;
 printf("\nSoma Total: %d + %d = %d\n", num1, num2, soma);
 
+printf("This is a cool branch test!");
+//braaanchOne!!!!!!!!!!!!!!
+
 return 0; }
