a4ce9a88bcbde2f413d0c5a2e174347b828a16fa
Cauê Gayoso Tavares da Silva <cauegts11@gmail.com>
2026-03-11 12:01:46 -0300

    teste

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 
 int main() { int num1, num2, soma; 
 
-printf("Digite o primeiro número:" );
+printf("Digite o primeiro número: ");
 scanf("%d", &num1);
-printf("Digite o segundo número:" );
+printf("Digite o segundo número: ");
 scanf("%d", &num2);
 soma = num1 + num2;
-printf("Soma Total: %d + %d = %d", num1, num2, soma);
+printf("\nSoma Total: %d + %d = %d\n", num1, num2, soma);
 
 return 0; }
